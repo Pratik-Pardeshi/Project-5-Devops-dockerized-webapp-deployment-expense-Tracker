6a8cc8e031dfd4d1dc8dadbcfecf5b6d6474faba
Pratik Pardeshi <89245723+Pratik-Pardeshi@users.noreply.github.com>
2024-07-28 11:29:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('button');
     button.addEventListener('click', () => {
-        alert('Welcome to the Ultra High-Level WebApp!');
+        alert('Hi Pratik Here, welcome to my DevOps WebApps Project 5...!');
     });
 });
